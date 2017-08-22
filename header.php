c59c6fca653e6205c478b6b28f361cfca0865254
Shyan Kashani <shyankashani1@gmail.com>
2017-08-22 15:31:29 -0700

    Added favicon

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 	<meta name=viewport content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900" rel="stylesheet">
+	<link rel="shortcut icon" type="image/png" href="img/favicon.png">
 </head>
 <body>
 	<script>
